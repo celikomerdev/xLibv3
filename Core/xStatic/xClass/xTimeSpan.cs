16faa67b6252db5b2e4728e3cffb4cbf8f4e8361
celikomerdev <celikomerdev@gmail.com>
2019-08-06 12:57:54 +0300

    xTimeSpan added

---
+++ Core/xStatic/xClass/xTimeSpan.cs
@@ -0,0 +1,20 @@
+﻿#if xLibv3
+using System;
+
+namespace xLib
+{
+	[Serializable]
+	public class xTimeSpan
+	{
+		public static string ToStringCustom(TimeSpan timeSpan,string format)
+		{
+			string temp = format;
+			temp = temp.Replace("dd",timeSpan.Days.ToString("00"));
+			temp = temp.Replace("hh",timeSpan.Hours.ToString("00"));
+			temp = temp.Replace("mm",timeSpan.Minutes.ToString("00"));
+			temp = temp.Replace("ss",timeSpan.Seconds.ToString("00"));
+			return temp;
+		}
+	}
+}
+#endif
