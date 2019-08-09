e0af4414b2bb5eb1dd5cf9314ae99bfc85ee04cb
celikomerdev <celikomerdev@gmail.com>
2019-08-09 14:39:38 +0300

    MnSafeTime++

---
+++ Core/xStatic/xClass/xDateTime.cs
@@ -0,0 +1,46 @@
+﻿#if xLibv3
+using System;
+using UnityEngine;
+
+namespace xLib
+{
+	[Serializable]
+	public struct xDateTime
+	{
+		[SerializeField]private int year;
+		[SerializeField]private int month;
+		[SerializeField]private int day;
+		[SerializeField]private int hour;
+		[SerializeField]private int minute;
+		[SerializeField]private int second;
+		public DateTime dateTime;
+		
+		public xDateTime(DateTime dateTime)
+		{
+			this.year = dateTime.Year;
+			this.month = dateTime.Month;
+			this.day = dateTime.Day;
+			this.hour = dateTime.Hour;
+			this.minute = dateTime.Minute;
+			this.second = dateTime.Second;
+			this.dateTime = dateTime;
+		}
+		
+		public void Init()
+		{
+			this.dateTime = new DateTime(year,month,day,hour,minute,second);
+		}
+		
+		#region StartOfDateTime
+		public static DateTime StartOfWeek(DateTime dateTime,DayOfWeek startDayOfWeek=DayOfWeek.Monday)
+		{
+			int deltaDay = 0;
+			deltaDay = dateTime.DayOfWeek-startDayOfWeek;
+			deltaDay += 7;
+			deltaDay %= 7;
+			return dateTime.AddDays(-deltaDay).Date;
+		}
+		#endregion
+	}
+}
+#endif
---
+++ Manager/MnSafeTime/MnSafeTime.cs
@@ -0,0 +1,35 @@
+﻿#if xLibv3
+using System;
+using UnityEngine;
+using xLib.EventClass;
+
+namespace xLib
+{
+	public class MnSafeTime : SingletonM<MnSafeTime>
+	{
+		[SerializeField]private xDateTime dateTimeDefault;
+		[SerializeField]private EventUnity onRefresh = new EventUnity();
+		
+		private DateTime dateTimeAnchor;
+		public DateTime DateTimeUtc
+		{
+			set
+			{
+				if(CanDebug) Debug.LogFormat(this,this.name+"DateTimeUtc:{0}",value.ToString());
+				dateTimeAnchor = value.AddSeconds(-Time.unscaledTime);
+				onRefresh.Invoke();
+			}
+			get
+			{
+				return dateTimeAnchor.AddSeconds(Time.unscaledTime);
+			}
+		}
+		
+		protected override void Awaked()
+		{
+			dateTimeDefault.Init();
+			DateTimeUtc = dateTimeDefault.dateTime;
+		}
+	}
+}
+#endif
