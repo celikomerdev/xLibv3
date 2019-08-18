7998c3e1c65818383e88e45f157613211f7a3806
celikomerdev <celikomerdev@gmail.com>
2019-08-18 14:57:26 +0300

    NodeGroup++

--- CustomGroup/NodeGroup.cs
+++ CustomGroup/NodeGroup.cs
@@ -1,112 +1,23 @@
 ﻿#if xLibv3
 using UnityEngine;
-using UnityEngine.Events;
 using xLib.xValueClass;
 
 namespace xLib.xNode.NodeObject
 {
-	public abstract class NodeGroup : NodeSave, ICall
+	public abstract class NodeGroup : NodeValue<ObjectGroup>
 	{
-		protected abstract ValueGroup Node
-		{
-			get;
-		}
-		
-		protected override void SetDebug()
-		{
-			Node.nodeSetting.canDebug = CanDebug;
-			Node.nodeSetting.objDebug = this;
-			if(string.IsNullOrEmpty(Key)) KeyGuid();
-		}
-		
-		public override string Key
-		{
-			get
-			{
-				return Node.nodeSetting.Key;
-			}
-			protected set
-			{
-				Node.nodeSetting.Key = value;
-			}
-		}
-		
-		public override string Name
-		{
-			get
-			{
-				return Node.nodeSetting.Name;
-			}
-		}
-		
-		protected override void OnInit(bool init)
-		{
-			Node.Init(init);
-		}
-		
-		#region ISerializableBase
-		public override bool UseRpc
-		{
-			set
-			{
-				Node.nodeSetting.UseRpc = value;
-			}
-		}
-		
-		public override string RpcTarget
-		{
-			set
-			{
-				Node.nodeSetting.RpcTarget = value;
-			}
-		}
-		
-		public override object SerializedObjectRaw
-		{
-			get
-			{
-				return Node.SerializedObjectRaw;
-			}
-			set
-			{
-				Node.SerializedObjectRaw = value;
-			}
-		}
-		
-		public override object SerializedObject
-		{
-			get
-			{
-				return Node.SerializedObject;
-			}
-			set
-			{
-				Node.SerializedObject = value;
-			}
-		}
-		
-		public override object SerializedObjectName
-		{
-			get
-			{
-				return Node.SerializedObjectName;
-			}
-		}
-		#endregion
-		
-		
 		#region ISerializableBaseContext
 		#if UNITY_EDITOR
-		[ContextMenu ("ChildKey = Name")]
+		[ContextMenu("ChildKey = Name")]
 		private void ChildKeyName()
 		{
-			Node.ChildKeyName();
+			Node.Value.ChildKeyName();
 		}
 		
-		[ContextMenu ("ChildKey = Guid")]
+		[ContextMenu("ChildKey = Guid")]
 		private void ChildKeyGuid()
 		{
-			Node.ChildKeyGuid();
+			Node.Value.ChildKeyGuid();
 		}
 		#endif
 		#endregion
@@ -117,46 +28,30 @@ namespace xLib.xNode.NodeObject
 		{
 			get
 			{
-				return Node.indexCurrent;
+				return Node.Value.indexCurrent;
 			}
 		}
 		
 		public ISerializableObject GetByIndex(int value)
 		{
-			return Node.GetByIndex(value);
+			return Node.Value.GetByIndex(value);
 		}
 		
 		public ISerializableObject GetByOrder(int value)
 		{
-			return Node.GetByOrder(value);
+			return Node.Value.GetByOrder(value);
 		}
 		
 		public ISerializableObject GetByRandom()
 		{
-			return Node.GetByRandom();
+			return Node.Value.GetByRandom();
 		}
 		
 		public ISerializableObject GetByKey(string value)
 		{
-			return Node.GetByKey(value);
+			return Node.Value.GetByKey(value);
 		}
 		#endregion
-		
-		
-		public override void Call()
-		{
-			Node.Call();
-		}
-		
-		public void ListenerCall(bool register,UnityAction call,bool onRegister=false)
-		{
-			Node.ListenerCall(register,call,onRegister);
-		}
-		
-		public void ListenerEditor(bool addition,BaseActiveM call)
-		{
-			Node.ListenerEditor(addition,call);
-		}
 	}
 }
 #endif
