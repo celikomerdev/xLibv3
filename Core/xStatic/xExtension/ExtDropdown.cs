ebfd244bb960668a427eb9c022f90e473ddcb153
celikomerdev <celikomerdev@gmail.com>
2019-08-19 14:10:40 +0300

    Dropdown extension added

---
+++ Core/xStatic/xExtension/ExtDropdown.cs
@@ -0,0 +1,26 @@
+﻿#if xLibv3
+using UnityEngine.UI;
+
+namespace xLib
+{
+	public static class ExtDropdown
+	{
+		public static void SetValueWithString(this Dropdown dropdown,string stringValue)
+		{
+			dropdown.value = dropdown.FindOptionIndex(stringValue);
+		}
+		
+		public static int FindOptionIndex(this Dropdown dropdown,string stringValue)
+		{
+			for (int i = 0; i < dropdown.options.Count; i++)
+			{
+				if (dropdown.options[i].text == stringValue)
+				{
+					return i;
+				}
+			}
+			return 0;
+		}
+	}
+}
+#endif
