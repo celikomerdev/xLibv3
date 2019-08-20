5e2551a6364bf5b5644a5c08a74ac61d55ead02d
celikomerdev <celikomerdev@gmail.com>
2019-08-20 12:08:58 +0300

    ++

--- Core/xStatic/xExtension/ExtTimeSpan.cs
+++ Core/xStatic/xExtension/ExtTimeSpan.cs
@@ -14,6 +14,9 @@ namespace xLib
 			temp = temp.Replace("mm",timeSpan.Minutes.ToString("00"));
 			temp = temp.Replace("ss",timeSpan.Seconds.ToString("00"));
 			return temp;
+			
+			// string format = @"dd\:hh\:mm\:ss";
+			// string formatted = timeSpan.ToString(@format);
 		}
 	}
 }
