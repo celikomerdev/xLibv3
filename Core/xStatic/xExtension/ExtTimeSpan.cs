585224d6e30c7c06b551003bca461388663cf7e1
celikomerdev <celikomerdev@gmail.com>
2019-08-20 11:19:14 +0300

    timespan HH++

--- Core/xStatic/xExtension/ExtTimeSpan.cs
+++ Core/xStatic/xExtension/ExtTimeSpan.cs
@@ -9,7 +9,7 @@ namespace xLib
 		{
 			string temp = format;
 			temp = temp.Replace("dd",timeSpan.Days.ToString("00"));
-			temp = temp.Replace("HH",timeSpan.TotalHours.ToString("00"));
+			temp = temp.Replace("HH",((int)timeSpan.TotalHours).ToString("00"));
 			temp = temp.Replace("hh",timeSpan.Hours.ToString("00"));
 			temp = temp.Replace("mm",timeSpan.Minutes.ToString("00"));
 			temp = temp.Replace("ss",timeSpan.Seconds.ToString("00"));
