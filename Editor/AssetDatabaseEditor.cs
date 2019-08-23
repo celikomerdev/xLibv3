0d7cb021fe41835e5cc5f1e5d8722caa71eab0a5
celikomerdev <celikomerdev@gmail.com>
2019-08-23 12:47:20 +0300

    ++

---
+++ Editor/AssetDatabaseEditor.cs
@@ -0,0 +1,28 @@
+﻿#if xLibv3
+using UnityEditor;
+using UnityEngine;
+
+namespace xLib
+{
+	public class AssetDatabaseEditor
+	{
+		[MenuItem("xLib/xAssetDatabase/SetDirty")]
+		public static void KeyGuid()
+		{
+			Object[] selectionArray = Selection.GetFiltered<Object>(SelectionMode.Unfiltered);
+			foreach(Object selection in selectionArray)
+			{
+				UnityEditor.EditorUtility.SetDirty(selection);
+			}
+			AssetDatabaseEditor.SaveAssets();
+		}
+		
+		[MenuItem("xLib/xAssetDatabase/SaveAssets")]
+		public static void SaveAssets()
+		{
+			Debug.LogFormat("xAssetDatabaseEditor:SaveAssets");
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
+#endif
---
+++ Editor/IDebugEditor.cs
@@ -0,0 +1,36 @@
+﻿#if xLibv3
+using UnityEditor;
+using UnityEngine;
+
+namespace xLib
+{
+	public class IDebugEditor
+	{
+		[MenuItem("xLib/IDebug/True")]
+		public static void SetTrue()
+		{
+			SetValue(true);
+		}
+		
+		[MenuItem("xLib/IDebug/False")]
+		public static void SetFalse()
+		{
+			SetValue(false);
+		}
+		
+		private static void SetValue(bool value)
+		{
+			Object[] arrayObject = Selection.GetFiltered<Object>(SelectionMode.Unfiltered);
+			IDebug[] arrayGeneric = arrayObject.GetGenericsInChildrenArray<IDebug>(true);
+			
+			for (int i = 0; i < arrayGeneric.Length; i++)
+			{
+				arrayGeneric[i].CanDebug = value;
+			}
+			
+			Debug.LogFormat("count:{0}",arrayGeneric.Length);
+			AssetDatabaseEditor.SaveAssets();
+		}
+	}
+}
+#endif
