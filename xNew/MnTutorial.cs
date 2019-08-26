671c9cdbc603dbd993a22a2d3a03545eddbe343d
celikomerdev <celikomerdev@gmail.com>
2019-08-26 09:28:04 +0300

    refolder++

---
+++ xNew/MnTutorial.cs
@@ -0,0 +1,15 @@
+﻿#if xLibv3
+using xLib.xNode.NodeObject;
+
+namespace xLib
+{
+	public class MnTutorial : SingletonM<MnTutorial>
+	{
+		public NodeString nodeMessageString;
+		public NodeTransform tutorialTransformPoint;
+		public NodeVoid onTutorialClick;
+		
+		
+	}
+}
+#endif
