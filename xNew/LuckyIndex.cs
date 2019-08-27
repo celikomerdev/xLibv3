92059f2c4b6acc06fa188b54684956224c014547
celikomerdev <celikomerdev@gmail.com>
2019-08-27 10:26:39 +0300

    ++

---
+++ xNew/LuckyIndex.cs
@@ -0,0 +1,59 @@
+﻿#if xLibv3
+using UnityEngine;
+using xLib.EventClass;
+
+namespace xLib.ToolRandom
+{
+	[System.Serializable]public class LuckyIndex : BaseWorkM
+	{
+		[SerializeField]private float[] arrayLuck;
+		
+		public int GetRandom()
+		{
+			if(LuckMax==0) LuckMax = 0;
+			float random = Random.Range(0,LuckMax);
+			if(CanDebug) Debug.LogFormat(this,this.name+":Random:{0}",random);
+			
+			int index = 0;
+			float totalLuck = 0;
+			
+			for (int i=0; i<arrayLuck.Length; i++)
+			{
+				totalLuck += arrayLuck[i];
+				
+				if(totalLuck > random)
+				{
+					index = i;
+					break;
+				}
+			}
+			
+			if(CanDebug) Debug.LogFormat(this,this.name+":Index:{0}",index);
+			return index;
+		}
+		
+		private float luckMax;
+		public float LuckMax
+		{
+			get
+			{
+				return luckMax;
+			}
+			set
+			{
+				luckMax = value;
+				for (int i = 0; i < arrayLuck.Length; i++)
+				{
+					luckMax += arrayLuck[i];
+				}
+			}
+		}
+		
+		public EventInt eventInt;
+		public void Call()
+		{
+			eventInt.Invoke(GetRandom());
+		}
+	}
+}
+#endif
