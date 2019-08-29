893b87beccf5ae990926c8577041698e7d914edd
celikomerdev <celikomerdev@gmail.com>
2019-08-29 18:45:24 +0300

    ++

---
+++ libScript/xManager/MnPopupWindow/MnPopupWindow.cs
@@ -0,0 +1,101 @@
+﻿#if xLibv3
+using UnityEngine;
+using UnityEngine.UI;
+using xLib.xNode.NodeObject;
+using xLib.xTweener;
+
+namespace xLib
+{
+	public class MnPopupWindow : SingletonM<MnPopupWindow>
+	{
+		[SerializeField]private Tweener window;
+		
+		#region Result
+		public NodeBool result;
+		public void Result(bool value)
+		{
+			window.enabled = false;
+			result.Value = value;
+		}
+		#endregion
+		
+		
+		#region Show
+		public void Reset()
+		{
+			Header("");
+			Body("");
+			Accept("");
+		}
+		
+		public void Show()
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Show");
+			window.enabled = true;
+		}
+		#endregion
+		
+		
+		
+		#region Header
+		[SerializeField]private Text header;
+		public void Header(string value)
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Header:{0}",value);
+			header.text = value;
+		}
+		
+		public void HeaderLocalized(string value)
+		{
+			Header(MnLocalize.GetValue(value));
+		}
+		#endregion
+		
+		#region Body
+		[SerializeField]private Text body;
+		public void Body(string value)
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Body:{0}",value);
+			body.text = value;
+		}
+		
+		public void BodyLocalized(string value)
+		{
+			Body(MnLocalize.GetValue(value));
+		}
+		#endregion
+		
+		#region Accept
+		[SerializeField]private Text accept;
+		[SerializeField]private GameObject objAccept;
+		public void Accept(string value)
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Accept:{0}",value);
+			objAccept.SetActive(!string.IsNullOrEmpty(value));
+			accept.text = value;
+		}
+		
+		public void AcceptLocalized(string value)
+		{
+			Accept(MnLocalize.GetValue(value));
+		}
+		#endregion
+		
+		#region Decline
+		[SerializeField]private Text decline;
+		[SerializeField]private GameObject objDecline;
+		public void Decline(string value)
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Decline:{0}",value);
+			objDecline.SetActive(!string.IsNullOrEmpty(value));
+			decline.text = value;
+		}
+		
+		public void DeclineLocalized(string value)
+		{
+			Decline(MnLocalize.GetValue(value));
+		}
+		#endregion
+	}
+}
+#endif
---
+++ libScript/xManager/MnPopupWindow/StPopupWindow.cs
@@ -0,0 +1,104 @@
+﻿#if xLibv3
+using UnityEngine.Events;
+
+namespace xLib
+{
+	public static class StPopupWindow
+	{
+		#region Result
+		public static void Listener(UnityAction<bool> call,bool register)
+		{
+			if(MnPopupWindow.ins==null) return;
+			int tempOrder = BaseRegisterM.Order;
+			BaseRegisterM.Order = 0;
+			MnPopupWindow.ins.result.Listener(register,call);
+			BaseRegisterM.Order = tempOrder;
+		}
+		
+		public static bool autoAccept = false;
+		public static void Result(bool value)
+		{
+			autoAccept = false;
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Result(value);
+		}
+		#endregion
+		
+		
+		#region Show
+		public static void Reset()
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Reset();
+		}
+		
+		public static void Show()
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Show();
+			
+			if(autoAccept) Result(true);
+		}
+		#endregion
+		
+		
+		#region Header
+		public static void Header(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Header(value);
+		}
+		
+		public static void HeaderLocalized(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.HeaderLocalized(value);
+		}
+		#endregion
+		
+		
+		#region Body
+		public static void Body(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Body(value);
+		}
+		
+		public static void BodyLocalized(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.BodyLocalized(value);
+		}
+		#endregion
+		
+		
+		#region Accept
+		public static void Accept(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Accept(value);
+		}
+		
+		public static void AcceptLocalized(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.AcceptLocalized(value);
+		}
+		#endregion
+		
+		#region Accept
+		public static void Decline(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.Decline(value);
+		}
+		
+		public static void DeclineLocalized(string value)
+		{
+			if(MnPopupWindow.ins==null) return;
+			MnPopupWindow.ins.DeclineLocalized(value);
+		}
+		#endregion
+	}
+}
+#endif
