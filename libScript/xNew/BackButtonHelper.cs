7fdc14795cf966e6c15b0d94da006f620c1f9b0e
celikomerdev <celikomerdev@gmail.com>
2019-08-29 12:10:36 +0300

    ++

---
+++ libScript/xNew/BackButtonHelper.cs
@@ -0,0 +1,38 @@
+﻿#if xLibv3
+using System.Collections.Generic;
+using UnityEngine;
+using xLib.EventClass;
+
+namespace xLib.xNew
+{
+	public class BackButtonHelper : BaseRegisterM
+	{
+		private static List<BackButtonHelper> list = new List<BackButtonHelper>();
+		
+		protected override bool OnRegister(bool value)
+		{
+			if(value) list.Add(this);
+			else list.Remove(this);
+			return value;
+		}
+		
+		public void CallFirst()
+		{
+			if(list.Count==0) return;
+			list[0].Call();
+		}
+		
+		public void CallLast()
+		{
+			if(list.Count==0) return;
+			list[list.Count-1].Call();
+		}
+		
+		[SerializeField]private EventUnity onCall = new EventUnity();
+		private void Call()
+		{
+			onCall.Invoke();
+		}
+	}
+}
+#endif
