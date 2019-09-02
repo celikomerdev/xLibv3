a858cc85d4bbfe7db25bd9fbe6b9bbe5d388c334
celikomerdev <celikomerdev@gmail.com>
2019-09-02 12:43:01 +0300

    ++

---
+++ libScript/xManager/MnTestGroup/MnTestGroup.cs
@@ -0,0 +1,34 @@
+﻿#if xLibv3
+using UnityEngine;
+using xLib.xNode.NodeObject;
+
+namespace xLib
+{
+	public class MnTestGroup : SingletonM<MnTestGroup>
+	{
+		protected override void Started()
+		{
+			Work();
+		}
+		
+		#region Work
+		private void Work()
+		{
+			if(CanDebug) Debug.LogFormat(this,this.name+":Work");
+			WorkInt();
+		}
+		
+		public NodeInt[] nodeInt;
+		private void WorkInt()
+		{
+			for (int i = 0; i < nodeInt.Length; i++)
+			{
+				#if GuruAnalytics
+				nodeInt[i].Value = Gameguru.Analytics.Analytics.GetGroupForABTest(nodeInt[i].Key);
+				#endif
+			}
+		}
+		#endregion
+	}
+}
+#endif
