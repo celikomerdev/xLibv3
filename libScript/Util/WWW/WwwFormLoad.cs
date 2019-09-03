fe028c3a9a6a259444b3da9e8d1ec0860334dfdd
celikomerdev <celikomerdev@gmail.com>
2019-09-03 16:27:58 +0300

    ++

---
+++ libScript/Util/WWW/WwwFormLoad.cs
@@ -0,0 +1,81 @@
+﻿#if xLibv3
+// #if ModWebWWW
+using System.Collections;
+using UnityEngine;
+using xLib.EventClass;
+
+namespace xLib
+{
+	public class WwwFormLoad : BaseM
+	{
+		[Header("ISerializableObject")]
+		[SerializeField]private Object[] arrayObjectKey = new Object[0];
+		[SerializeField]private Object[] arrayObjectName = new Object[0];
+		
+		[Header("Event")]
+		[SerializeField]private EventWWW eventWWW = new EventWWW();
+		
+		#region Url
+		private ValueBase<string> url = new ValueMulti<string>();
+		public string Url
+		{
+			set
+			{
+				if(string.IsNullOrEmpty(value)) return;
+				if(url.Value == value) return;
+				url.Value = value;
+				MnCoroutine.NewCoroutine(eDownload(ViewCore.CurrentId,value));
+			}
+		}
+		#endregion
+		
+		#region Download
+		private IEnumerator eDownload(string invokeId,string url)
+		{
+			WWW www = new WWW(url,FormData);
+			yield return www;
+			
+			if (string.IsNullOrEmpty(www.error))
+			{
+				string tempId = ViewCore.CurrentId;
+				ViewCore.CurrentId = invokeId;
+				eventWWW.Invoke(www);
+				ViewCore.CurrentId = tempId;
+			}
+			else
+			{
+				xDebug.LogExceptionFormat(this,this.name+":Error:{0}:{1}",www.error,url);
+			}
+			
+			www.Dispose();
+			www = null;
+			yield return null;
+		}
+		#endregion
+		
+		
+		private WWWForm FormData
+		{
+			get
+			{
+				WWWForm form = new WWWForm();
+				
+				ISerializableObject[] iSerializableObjectKey = arrayObjectKey.GetGenericsArray<ISerializableObject>();
+				for (int i = 0; i < iSerializableObjectKey.Length; i++)
+				{
+					form.AddField(iSerializableObjectKey[i].Key,(string)iSerializableObjectKey[i].SerializedObjectRaw);
+				}
+				
+				ISerializableObject[] iSerializableObjectName = arrayObjectName.GetGenericsArray<ISerializableObject>();
+				for (int i = 0; i < iSerializableObjectName.Length; i++)
+				{
+					form.AddField(iSerializableObjectName[i].Name,(string)iSerializableObjectName[i].SerializedObjectName);
+				}
+				
+				return form;
+			}
+		}
+	}
+}
+// #endif
+#endif
