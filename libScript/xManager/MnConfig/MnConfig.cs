4b8f1dd3453778ece12e7653601e8c7b93defcdf
celikomerdev <celikomerdev@gmail.com>
2019-09-11 16:11:21 +0300

    ++

---
+++ libScript/xManager/MnConfig/MnConfig.cs
@@ -0,0 +1,13 @@
+﻿#if xLibv3
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace xLib
+{
+	public class MnConfig : SingletonM<MnConfig>
+	{
+		public static JObject data = null;
+		public static Action onUpdateData;
+	}
+}
+#endif
