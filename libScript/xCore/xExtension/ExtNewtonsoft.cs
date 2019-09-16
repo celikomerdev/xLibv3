b9d77738c8a6360e2865a892dcb1b5d1194f36d6
celikomerdev <celikomerdev@gmail.com>
2019-09-16 13:05:53 +0300

    ++

---
+++ libScript/xCore/xExtension/ExtNewtonsoft.cs
@@ -0,0 +1,29 @@
+﻿#if xLibv3
+using Newtonsoft.Json.Linq;
+
+namespace xLib
+{
+	public static class ExtNewtonsoft
+	{
+		public static T GetValueSafe<T>(this JToken jToken,object key,T defaultValue)
+		{
+			if(jToken == null) return defaultValue;
+			
+			JToken valueToken = jToken[key];
+			T returnValue = valueToken.Type == JTokenType.Null? defaultValue:valueToken.Value<T>();
+			
+			return returnValue;
+		}
+		
+		public static T SelectTokenSafe<T>(this JObject jObject,string path,T defaultValue)
+		{
+			if(jObject == null) return defaultValue;
+			JToken valueToken = jObject.SelectToken(path);
+			if(valueToken == null) return defaultValue;
+			return valueToken.ToObject<T>();
+		}
+	}
+}
+#endif
+
+// JToken vehicle = MnConfig.data.SelectToken($"$.arrayVehicle[?(@.id == '{carData.model}')]");
--- libScript/xManager/MnConfig/MnConfig.cs
+++ libScript/xManager/MnConfig/MnConfig.cs
@@ -1,13 +1,24 @@
 ﻿#if xLibv3
 using System;
 using Newtonsoft.Json.Linq;
+using UnityEngine;
 
 namespace xLib
 {
 	public class MnConfig : SingletonM<MnConfig>
 	{
+		public static TextAsset assetConfigData;
 		public static JObject data = null;
 		public static Action onUpdateData;
+		
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void InitStatic()
+		{
+			Debug.Log("MnConfig");
+			assetConfigData = Resources.Load<TextAsset>("ConfigData");
+			data = JObject.Parse(assetConfigData.text);
+			onUpdateData?.Invoke();
+		}
 	}
 }
 #endif
