a5413bc4825a1539251df13982937f034356b40f
celikomerdev <celikomerdev@gmail.com>
2019-09-18 16:09:16 +0300

    ++

--- libScript/xUtil/CooldownLoop.cs
+++ libScript/xUtil/CooldownLoop.cs
@@ -8,16 +8,25 @@ namespace xLib.xNew
 	public class CooldownLoop : BaseTickNodeM
 	{
 		[Header("Target")]
-		[SerializeField]public long cooldown = 600000000;
+		[SerializeField]private long cooldown = 600000000;
+		public long Cooldown
+		{
+			get
+			{
+				return cooldown;
+			}
+			set
+			{
+				cooldown = value;
+				this.enabled = (cooldown>0);
+			}
+		}
 		[SerializeField]public NodeLong currentTime = null;
 		[SerializeField]public NodeLong nextTime = null;
 		
 		[Header("Work")]
 		[SerializeField]private int maxLoop = 1;
 		[SerializeField]private EventUnity eventLoop = new EventUnity();
-		[SerializeField]private EventBool eventReady = new EventBool();
-		
-		private long deltaTick = 0;
 		[SerializeField]private EventLong eventDeltaTick = new EventLong();
 		
 		
@@ -31,47 +40,39 @@ namespace xLib.xNew
 			if(!CanWork) return;
 			if(currentTime.Value == 0) return;
 			
-			deltaTick = nextTime.Value-currentTime.Value;
+			long deltaTick = nextTime.Value-currentTime.Value;
 			if(CanDebug) Debug.LogFormat(this,this.name+":deltaTick:{0}",deltaTick);
 			
 			if(deltaTick>0)
 			{
-				eventReady.Invoke(false);
 				eventDeltaTick.Invoke(deltaTick);
 				return;
 			}
+			this.enabled = false;
 			
-			if(deltaTick>-cooldown)
+			int currentLoop = 0;
+			while(currentTime.Value > nextTime.Value)
 			{
-				if(CanDebug) Debug.LogFormat(this,this.name+":NoLoop:{0}",deltaTick);
+				currentLoop++;
+				if(currentLoop < maxLoop) nextTime.Value += cooldown;
+				else nextTime.Value = currentTime.Value+cooldown;
+				
+				if(CanDebug) Debug.LogFormat(this,this.name+":Reward:{0}",currentLoop);
+				eventLoop.Invoke();
 			}
-			else
-			{
-				int currentLoop = 0;
-				while(currentTime.Value > nextTime.Value)
-				{
-					currentLoop++;
-					if(currentLoop < maxLoop) nextTime.Value += cooldown;
-					else nextTime.Value = currentTime.Value;
-					
-					if(CanDebug) Debug.LogFormat(this,this.name+":Reward:{0}",currentLoop);
-					eventLoop.Invoke();
-				}
-				if(nextTime.Value>currentTime.Value) nextTime.Value = currentTime.Value;
-			}
-			
-			eventReady.Invoke(true);
 		}
 		
-		public void StartCooldown()
+		public void CooldownReStart()
 		{
 			nextTime.Value = currentTime.Value+cooldown;
+			this.enabled = true;
 			Call();
 		}
 		
 		public void TickAdd(long value)
 		{
 			nextTime.Value += value;
+			this.enabled = true;
 			Call();
 		}
 		
