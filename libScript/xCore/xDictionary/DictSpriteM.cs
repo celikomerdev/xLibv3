8c4f82e552cf45766f734678fc10e23f1313e417
celikomerdev <celikomerdev@gmail.com>
2019-09-19 12:03:56 +0300

    ++

--- libScript/xCore/xDictionary/DictSpriteM.cs
+++ libScript/xCore/xDictionary/DictSpriteM.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 namespace xLib.xDictionary
 {
+	public class DictSpriteM : BaseInitM
+	{
+		public DictSprite dictSprite = new DictSprite();
+		
+		protected override void OnInit(bool init)
+		{
+			if(init) dictSprite.Init();
+		}
+	}
+	
 	[Serializable]public class DictSprite : DictKV<string,Sprite,StringSprite>{}
 	[Serializable]public class StringSprite : ObjectKV<string,Sprite>{}
 }
