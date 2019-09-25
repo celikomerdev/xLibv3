fa2bc7e10d5eee2b01d509da4bbc9ea121e4d0e0
celikomerdev <celikomerdev@gmail.com>
2019-09-25 17:33:08 +0300

    ++

---
+++ libScript/xCore/xExtension/ExtDropdownPro.cs
@@ -0,0 +1,28 @@
+﻿#if xLibv3
+#if TMPro
+using TMPro;
+
+namespace xLib
+{
+	public static class ExtDropdownPro
+	{
+		public static void SetValueWithString(this TMP_Dropdown dropdown,string stringValue)
+		{
+			dropdown.value = dropdown.FindOptionIndex(stringValue);
+		}
+		
+		public static int FindOptionIndex(this TMP_Dropdown dropdown,string stringValue)
+		{
+			for (int i = 0; i < dropdown.options.Count; i++)
+			{
+				if (dropdown.options[i].text == stringValue)
+				{
+					return i;
+				}
+			}
+			return 0;
+		}
+	}
+}
+#endif
+#endif
