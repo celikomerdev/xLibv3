ef25388abd8965b5b1b7207d558b48ce368a4dac
celikomerdev <celikomerdev@gmail.com>
2019-11-04 14:01:59 +0300

    ++

---
+++ libScript/xTool/Component/xButton.cs
@@ -0,0 +1,26 @@
+﻿#if xLibv3
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace xLib
+{
+	public class xButton : BaseMainM
+	{
+		[SerializeField]private Button target = null;
+		
+		public void OnClick()
+		{
+			target.onClick.Invoke();
+		}
+		
+		#if UNITY_EDITOR
+		[ContextMenu("Fill")]
+		private void Fill()
+		{
+			if(!target) target = GetComponent<Button>();
+			if(!target) target = GetComponentInParent<Button>();
+		}
+		#endif
+	}
+}
+#endif
