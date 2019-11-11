6201615fda62a057fc6666f937c68151e78f5708
celikomerdev <celikomerdev@gmail.com>
2019-11-11 12:41:27 +0300

    ++

---
+++ libScript/xTool/Application/ApplicationIsEditor.cs
@@ -0,0 +1,17 @@
+﻿#if xLibv2
+using UnityEngine;
+using xLib.ToolEventClass;
+
+namespace xLib.ToolApplication
+{
+	public class ApplicationIsEditor : BaseM
+	{
+		public EventBool eventBool;
+		
+		private void Awake()
+		{
+			eventBool.Invoke(Application.isEditor);
+		}
+	}
+}
+#endif
---
+++ libScript/xTool/Application/ApplicationPlatform.cs
@@ -0,0 +1,27 @@
+﻿#if xLibv2
+using UnityEngine;
+using xLib.ToolEventClass;
+
+namespace xLib.ToolApplication
+{
+	public class ApplicationPlatform : BaseM
+	{
+		public RuntimePlatform[] array;
+		public EventBool eventBool;
+		
+		private void Awake()
+		{
+			eventBool.Invoke(IsValid());
+		}
+		
+		private bool IsValid()
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				if(array[i] == Application.platform) return true;
+			}
+			return false;
+		}
+	}
+}
+#endif
