68fe1f60c12684ee8de51777b376a5c2d9ddcc78
celikomerdev <celikomerdev@gmail.com>
2019-11-27 13:04:14 +0300

    ++

---
+++ libScript/xTool/Animator/AnimatorSetFloat.cs
@@ -0,0 +1,17 @@
+﻿#if xLibv3
+using UnityEngine;
+
+namespace xLib
+{
+	public class AnimatorSetFloat : BaseMainM
+	{
+		[SerializeField]private string key = "";
+		[SerializeField]private Animator target = null;
+		
+		public void Set(float value)
+		{
+			target.SetFloat(key,value);
+		}
+	}
+}
+#endif
