1fca7ea55b148357ab3eeb7a7cf8c5d80f6f4c99
celikomerdev <celikomerdev@gmail.com>
2019-12-26 13:03:30 +0300

    ++

---
+++ libPlugin/DeviceDisplay/DeviceDisplay.cs
@@ -0,0 +1,30 @@
+﻿#if xLibv3
+namespace xLib
+{
+	public static class DeviceDisplay
+	{
+		public static int scaleFactor
+		{
+			get
+			{
+				return _scaleFactor();
+			}
+		}
+		
+		#if UNITY_EDITOR
+		private static int _scaleFactor()
+		{
+			return 1;
+		}
+		#elif UNITY_IPHONE
+		[System.Runtime.InteropServices.DllImport("__Internal")]
+		private static extern int _scaleFactor();
+		#else
+		private static int _scaleFactor()
+		{
+			return 1;
+		}
+		#endif
+	}
+}
+#endif
