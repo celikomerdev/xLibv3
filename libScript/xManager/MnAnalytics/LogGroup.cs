7ba35d2a8c5205c0a9961c48eee995d606d5c67b
celikomerdev <celikomerdev@gmail.com>
2020-01-07 17:40:24 +0300

    ++

--- libScript/xManager/MnAnalytics/LogGroup.cs
+++ libScript/xManager/MnAnalytics/LogGroup.cs
@@ -1,10 +1,13 @@
 ﻿#if xLibv3
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace xLib.ToolManager
 {
 	public abstract class LogGroup : BaseWorkM
 	{
+		[SerializeField]private float interval = 0.1f;
 		[SerializeField]private Object[] arrayIAnalyticObject = new Object[0];
 		[SerializeField]private IAnalyticObject[] m_arrayIAnalyticObject = new IAnalyticObject[0];
 		
@@ -17,19 +20,33 @@ namespace xLib.ToolManager
 		public void SendAll()
 		{
 			if(CanDebug) Debug.Log($"{this.name}:Call",this);
+			List<IAnalyticObject> listDirty = new List<IAnalyticObject>();
 			for (int i = 0; i < m_arrayIAnalyticObject.Length; i++)
 			{
 				IAnalyticObject analyticObject = m_arrayIAnalyticObject[i];
-				if(analyticObject.AnalyticDirty) Send(analyticObject);
+				if(analyticObject.AnalyticDirty) listDirty.Add(analyticObject);
 			}
-			this.WaitForSeconds(call:ClearAll,delay:0.1f);
+			this.NewCoroutine(enumerator:SendDirty(listDirty));
 		}
 		
 		private void ClearAll()
 		{
+			if(CanDebug) Debug.Log($"{this.name}:ClearAll",this);
 			for (int i = 0; i < m_arrayIAnalyticObject.Length; i++)
 			{
 				m_arrayIAnalyticObject[i].AnalyticDirty = false;
+			}
+		}
+		
+		private IEnumerator SendDirty(List<IAnalyticObject> list)
+		{
+			yield return new WaitForSecondsRealtime(0.1f);
+			ClearAll();
+			
+			for (int i = 0; i < list.Count; i++)
+			{
+				yield return new WaitForSecondsRealtime(interval);
+				Send(list[i]);
 			}
 		}
 		
