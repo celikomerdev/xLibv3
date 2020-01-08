f312ae9e9665da2620e8493b75cad13bc22fdfed
celikomerdev <celikomerdev@gmail.com>
2020-01-08 17:56:03 +0300

    ++

--- libScript/xManager/SingletonM.cs
+++ libScript/xManager/SingletonM.cs
@@ -39,7 +39,7 @@ namespace xLib
 			if(!ins) return;
 			if(insBase.isPrimary && !isPrimary) return;
 			insBase.Destroy();
-			if(CanDebug) Debug.LogWarning($"{this.name}:Replaced",this);
+			Debug.LogWarning($"{this.name}:Replaced",this);
 		}
 		
 		protected virtual void Started(){}
@@ -82,14 +82,14 @@ namespace xLib
 		
 		private void Destroy()
 		{
-			if(CanDebug) Debug.LogWarning($"{this.name}:Destroy",this);
+			Debug.LogWarning($"{this.name}:Destroy",this);
 			if(ins==this) ins = null;
 			Destroy(this.gameObject);
 		}
 		
 		private void DestroyImmediate()
 		{
-			if(CanDebug) Debug.LogWarning($"{this.name}:DestroyImmediate",this);
+			Debug.LogWarning($"{this.name}:DestroyImmediate",this);
 			if(ins==this) ins = null;
 			DestroyImmediate(this.gameObject);
 		}
