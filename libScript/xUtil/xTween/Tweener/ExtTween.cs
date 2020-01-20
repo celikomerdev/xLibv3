b30c7fe1acad9a615df526df74abaf28676224b6
celikomerdev <celikomerdev@gmail.com>
2020-01-20 10:11:00 +0300

    ++

--- libScript/xUtil/xTween/Tweener/ExtTween.cs
+++ libScript/xUtil/xTween/Tweener/ExtTween.cs
@@ -7,8 +7,9 @@ namespace xLib
 {
 	public static class ExtTween
 	{
-		public static Coroutine Tween(UnityAction<float> call,float duration = 0.1f, bool ignoreTimeScale=true)
+		public static Coroutine Tween(UnityAction<float> call,float duration = 0.1f, bool ignoreTimeScale=true,AnimationCurve curve=null)
 		{
+			if(curve==null) curve = AnimationCurve.Linear(0,0,1,1);
 			return MnCoroutine.ins.NewCoroutine(Flow());
 			IEnumerator Flow()
 			{
@@ -17,6 +18,7 @@ namespace xLib
 				{
 					elapsedTime += ignoreTimeScale? Time.unscaledDeltaTime:Time.deltaTime;
 					float ratio = Mathf.Clamp01(elapsedTime/duration);
+					ratio = curve.Evaluate(ratio);
 					call.Invoke(ratio);
 					yield return null;
 				}
