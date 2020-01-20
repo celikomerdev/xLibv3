a7d1dd0128afae0c2033f771c3f3573d1f386096
celikomerdev <celikomerdev@gmail.com>
2020-01-20 13:02:50 +0300

    ++

--- libScript/xManager/MnTutorial/HighlightMaterial.cs
+++ libScript/xManager/MnTutorial/HighlightMaterial.cs
@@ -18,30 +18,15 @@ namespace xLib
 				this.enabled = (value!=null);
 			}
 		}
+		
 		[SerializeField]private Camera targetCamera = null;
 		[SerializeField]private Material targetMaterial = null;
-		
-		protected override bool OnRegister(bool register)
-		{
-			if(!register)
-			{
-				target.localScale = Vector3.zero;
-			}
-			else
-			{
-				target.localScale = Vector3.one;
-			}
-			target.localPosition = Vector3.zero;
-			Tick(1);
-			return base.OnRegister(register);
-		}
-		
 		protected override void Tick(float tickTime)
 		{
 			Vector2 screenPoint = targetCamera.WorldToScreenPoint(target.position);
 			Vector2 position = new Vector4(screenPoint.x / targetCamera.pixelWidth, (screenPoint.y + (targetCamera.pixelWidth - targetCamera.pixelHeight) / 2f) / targetCamera.pixelWidth);
 			targetMaterial.SetVector("_Center",position);
-			targetMaterial.SetFloat("_Radius",target.lossyScale.z*10f);
+			targetMaterial.SetFloat("_Radius",target.lossyScale.z);
 		}
 	}
 }
---
+++ libScript/xUtil/xTween/Tween/TweenMaterialVector2.cs
@@ -0,0 +1,19 @@
+﻿#if xLibv3
+using UnityEngine;
+
+namespace xLib.xTween
+{
+	public class TweenMaterialVector2 : Tween
+	{
+		[SerializeField]private Material material = null;
+		[SerializeField]private string propertyName = "";
+		[SerializeField]private Vector2 from = Vector2.zero;
+		[SerializeField]private Vector2 to = Vector2.one;
+		
+		protected override void SetRatio(float value)
+		{
+			material.SetVector(propertyName,Vector2.LerpUnclamped(from,to,value));
+		}
+	}
+}
+#endif
