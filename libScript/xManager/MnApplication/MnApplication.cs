17740fd7ad5f2a2c651441e6584457ee49277045
celikomerdev <celikomerdev@gmail.com>
2020-01-31 12:31:45 +0300

    ++

--- libScript/xManager/MnApplication/MnApplication.cs
+++ libScript/xManager/MnApplication/MnApplication.cs
@@ -2,9 +2,18 @@
 using UnityEngine;
 using xLib.xNode.NodeObject;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+#endif
+
 namespace xLib
 {
-	public class MnApplication : SingletonM<MnApplication>
+	public class MnApplication : SingletonM<MnApplication>,
+	#if UNITY_EDITOR
+	IPreprocessBuildWithReport
+	#endif
 	{
 		[Header("SystemInfo")]
 		[SerializeField]private NodeString SystemInfo_deviceUniqueIdentifier = null;
@@ -19,6 +28,8 @@ namespace xLib
 		[SerializeField]private NodeString Application_productName = null;
 		[SerializeField]private NodeString Application_sandboxType = null;
 		[SerializeField]private NodeString Application_version_name = null;
+		
+		[SerializeField]private int app_version = 0;
 		[SerializeField]private NodeInt Application_version = null;
 		
 		protected override void Awaked()
@@ -35,12 +46,38 @@ namespace xLib
 			Application_sandboxType.Value = Application.sandboxType.ToString();
 			Application_version_name.Value = Application.version;
 			
-			int app_version = 0;
-			if(int.TryParse(Application.version, out app_version))
+			#if UNITY_EDITOR
+			app_version = PlayerSettings.Android.bundleVersionCode;
+			#endif
+			
+			int app_version_parse = 0;
+			if(int.TryParse(Application.version, out app_version_parse))
 			{
-				Application_version.Value = app_version;
+				app_version = app_version_parse;
+			}
+			
+			Application_version.Value = app_version;
+		}
+		
+		
+		#if UNITY_EDITOR
+		int IOrderedCallback.callbackOrder
+		{
+			get
+			{
+				return 0;
 			}
 		}
+		
+		void IPreprocessBuildWithReport.OnPreprocessBuild(BuildReport report)
+		{
+			app_version = PlayerSettings.Android.bundleVersionCode;
+			Debug.Log($"{this.name}:OnPreprocessBuild:app_version:{app_version}",this);
+		}
+		#endif
 	}
 }
 #endif
+
+// string[] version = Application.version.Split('.');
+// var versionNumbered = Convert.ToInt32(version[0])*1000000 +Convert.ToInt32(version[1]) * 1000 + Convert.ToInt32(version[2]);
