a181583dd13551c770df774c188207ac49e7fb5d
celikomerdev <celikomerdev@gmail.com>
2020-02-12 18:16:58 +0300

    ++

---
+++ libScript/xManager/MnInput/Manager/InputUnity/InputGetAxis.cs
@@ -0,0 +1,18 @@
+﻿#if xLibv3
+using UnityEngine;
+using xLib.EventClass;
+
+namespace xLib.xTool.xInput
+{
+	public class InputGetAxis : BaseTickNodeM
+	{
+		[SerializeField]private string key = "";
+		[SerializeField]private EventFloat eventFloat = new EventFloat();
+		
+		protected override void Tick(float tickTime)
+		{
+			eventFloat.Invoke(Input.GetAxis(key));
+		}
+	}
+}
+#endif
--- libScript/xManager/MnInput/Manager/InputUnity/InputGetKeyDown.cs
+++ libScript/xManager/MnInput/Manager/InputUnity/InputGetKeyDown.cs
@@ -7,13 +7,12 @@ namespace xLib.xTool.xInput
 	public class InputGetKeyDown : BaseTickNodeM
 	{
 		[SerializeField]private string key = "";
-		[UnityEngine.Serialization.FormerlySerializedAs("eventBool")]
-		[SerializeField]private EventBool eventKeyDown = new EventBool();
+		[SerializeField]private EventBool eventBool = new EventBool();
 		
 		protected override void Tick(float tickTime)
 		{
-			if(Input.GetKeyDown(key)) eventKeyDown.Invoke(true);
-			else if(Input.GetKeyUp(key)) eventKeyDown.Invoke(false);
+			if(Input.GetKeyDown(key)) eventBool.Invoke(true);
+			else if(Input.GetKeyUp(key)) eventBool.Invoke(false);
 		}
 	}
 }
