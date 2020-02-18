dfa80476b1672bc8a43fa5c8f983457fc3410a45
celikomerdev <celikomerdev@gmail.com>
2020-02-18 15:55:07 +0300

    xLua test++

---
+++ libLua/LuaM.cs
@@ -0,0 +1,21 @@
+﻿#if xLibv3
+using UnityEngine;
+
+namespace xLib.libLua
+{
+	public class LuaM : BaseWorkM
+	{
+		[SerializeField]private TextAsset luaScript = null;
+		public void Call()
+		{
+			//initalize first!
+			xValueClass.ValueInt.GetGlobal("DefaultGlobal").Value = 5;
+			Debug.Log(xValueClass.ValueInt.GetGlobal("DefaultGlobal").Value);
+			
+			#if xLua
+			xLuaInit.luaEnv.DoString(luaScript.text);
+			#endif
+		}
+	}
+}
+#endif
---
+++ libLua/xLuaInit.cs
@@ -0,0 +1,20 @@
+﻿#if xLibv3
+#if xLua
+using UnityEngine;
+using XLua;
+
+namespace xLib.libLua
+{
+	public static class xLuaInit
+	{
+		public static LuaEnv luaEnv = null;
+		
+		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+		private static void InitEnvironment()
+		{
+			luaEnv = new LuaEnv();
+		}
+	}
+}
+#endif
+#endif
